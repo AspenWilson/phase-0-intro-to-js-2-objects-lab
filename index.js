a3c0b5f19e16d14ea578f9877eb01ba9af0468bf
Aspen Wilson <aspen.schell@gmail.com>
2023-01-15 15:23:11 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Sam',
+    streetAddress: {}
+}
+function updateEmployeeWithKeyAndValue() {
+    const employee1 = {
+        name: employee.name,
+        streetAddress: '11 Broadway'
+    }
+    return employee1;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress) {
+    employee[streetAddress] = '12 Broadway';
+    return employee
+}
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,name) {
+    delete employee.name;
+    return employee;
+}
+
